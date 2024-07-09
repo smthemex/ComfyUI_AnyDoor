a5d2120103bd0abc3fb901bb43187dcedfaa8d42
smthemex <138738845+smthemex@users.noreply.github.com>
2024-07-09 08:26:38 +0800

    Update model.py

--- ldmx/modules/diffusionmodules/model.py
+++ ldmx/modules/diffusionmodules/model.py
@@ -6,7 +6,7 @@ import numpy as np
 from einops import rearrange
 from typing import Optional, Any
 
-from ldm.modules.attention import MemoryEfficientCrossAttention
+from ..attention import MemoryEfficientCrossAttention
 
 try:
     import xformers
